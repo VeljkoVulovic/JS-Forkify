90ae6fe7c30cb707226178963189c6af5919a46b
Veljko VuloviÄc <veljkovulovic1@gmail.com>
2022-05-16 00:05:33 +0200

    Implementing Error and Success Messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -17,7 +17,7 @@ const controlRecipes = async function() {
     //2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
-    alert(error);
+    recipeView.renderError();
   }
 };
 
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,6 +4,8 @@ import { Fraction } from 'fractional';
 class recipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = 'We could not find that recipe. Please try another one!';
+  #message = '';
 
   render(data) {
     this.#data = data;
@@ -24,9 +26,39 @@ class recipeView {
           </svg>
         </div>
     `;
-    this.#parentElement.innerHTML = '';
+    this.#clear();
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   };
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+          <div class='error'>
+            <div>
+              <svg>
+                <use href='${icons}#icon-alert-triangle'></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `
+          <div class='message'>
+            <div>
+              <svg>
+                <use href='${icons}#icon-smile'></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 
   #generateMarkup() {
     return `
